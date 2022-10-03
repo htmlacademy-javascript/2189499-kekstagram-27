f9f2d89fdc3a609126fff47ba3efc1e228722138
Shura Mishura <collagememorycollage@gmail.com>
2022-10-03 21:39:10 +0300

    добавили функции в main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+// first function random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomIntInclusive();
 
+//second function
+function commentLength (str, maxLength) {
+  if ( str.length > maxLength) {
+    return true;
+  }
+  return false;
+}
+commentLength('kjdsklfjalksdjl', 10);
