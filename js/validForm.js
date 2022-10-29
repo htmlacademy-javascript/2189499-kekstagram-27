37b7520e16274b35820bd9c3f4cd0431e10eb4d8
Alex <collagememorycollage@gmail.com>
2022-10-30 02:18:47 +0300

    продолжаю работу над валидацией

--- js/validForm.js
+++ js/validForm.js
@@ -1,8 +1,8 @@
 
 
 const form = document.getElementById('upload-select-image');
-const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
-
+const hashtag = /^#[a-zа-яё0-9\s]{1,19}$/i;
+const commentValue = /.{1,140}$/i;
 console.log(hashtag.test());
 // create the pristine instance
 const pristine = new Pristine(form);
@@ -11,11 +11,26 @@ function isHashtagValid() {
   const input = document.querySelector('.text__hashtags').value;
   console.log(input);
   // настройки для валидации
+  const l = input.split(/\s/);
+  console.log(l);
+  
+  l.forEach((element, index, array) => {
+    //вот тут проверили что-то
+    console.log(hashtag.test(element));
+  });
   return hashtag.test(input) || input === '';
-  // return value.length >= 2 && value.length <= 50;
 };
 
 pristine.addValidator(form.querySelector('.text__hashtags'),isHashtagValid);
+
+
+// function isCommentValid() {
+//   const comment = document.querySelector('.text__description').value;
+//   console.log(comment);
+//   return comment <= 140;
+// };
+
+// pristine.addValidator(form.querySelector('.text__description'),isCommentValid);
 
 
 form.addEventListener('submit', (e) => {
