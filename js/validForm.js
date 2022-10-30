1355b420f4ac6507c0f95d7309c38ce3e30781e3
Alex <collagememorycollage@gmail.com>
2022-10-30 12:49:43 +0300

    добавили валидность для хештега

--- js/validForm.js
+++ js/validForm.js
@@ -1,24 +1,22 @@
 
 
 const form = document.getElementById('upload-select-image');
-const hashtag = /^#[a-zа-яё0-9\s]{1,19}$/i;
+const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
 const commentValue = /.{1,140}$/i;
 console.log(hashtag.test());
 // create the pristine instance
 const pristine = new Pristine(form);
 
-function isHashtagValid() {
+function isHashtagValid(ret) {
+
   const input = document.querySelector('.text__hashtags').value;
-  console.log(input);
-  // настройки для валидации
-  const l = input.split(/\s/);
-  console.log(l);
-  
-  l.forEach((element, index, array) => {
-    //вот тут проверили что-то
-    console.log(hashtag.test(element));
+  let b;
+  const arrayOfInput = input.split(/\s/);
+  arrayOfInput.forEach(element => {
+    b = hashtag.test(element);
+    ret = b;
   });
-  return hashtag.test(input) || input === '';
+  return ret || input === '';
 };
 
 pristine.addValidator(form.querySelector('.text__hashtags'),isHashtagValid);
@@ -45,33 +43,3 @@ form.addEventListener('submit', (e) => {
   };
 });
 
-
-
-// const form = document.querySelector('.img-upload__form');
-
-// // create the pristine instance
-// const pristine = new Pristine (form);
-// // check if the form is valid
-
-
-
-// const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
-
-// function isHashtagValid(value) {
-//   return value.length >= 2 && value.length <= 50;
-// };
-
-// pristine.addValidator(form.querySelector('#upload-file'),isHashtagValid,
-//   'От 2 до 50 символов'
-// );
-
-
-// form.addEventListener('submit', (evt) => {
-//   evt.preventDefault();
-//   const valid = pristine.validate();
-//   if (valid) {
-//     console.log('валидна');
-//   } else {
-//     console.log('нет');
-//   };
-// });
