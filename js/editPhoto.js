fda4111f976d1f8c0bcb441a84455643bfb98db9
Alex <collagememorycollage@gmail.com>
2022-11-03 01:01:51 +0300

    исправил ошибки в коде

--- js/editPhoto.js
+++ js/editPhoto.js
@@ -1,24 +1,25 @@
 const formBtn = document.querySelector('.img-upload__scale');
 const smallerBtn = formBtn.querySelector('.scale__control--smaller');
 const biggerBtn = formBtn.querySelector('.scale__control--bigger');
-const image = document.querySelector('.img-upload__preview');
+const image = document.querySelector('.img-upload__preview img');
 let valueBtn = formBtn.querySelector('.scale__control--value');
 
 const SCALE_STEP = 25;
 const MIN_SCALE = 25;
-const MAX_SCALW = 100;
+const MAX_SCALE = 100;
 const DEFAULT_SCALE = 100;
 
 function scaleImage (value = DEFAULT_SCALE) {
-    image.style.transform = `scace(${value / 100})`;
+    image.style.transform = `scale(${value / 100})`;
+    console.log(image.style.transform);
     valueBtn.value = `${value}%`;
 };
 
 function OnBiggerButtonClick() {
     const currentValue = parseInt(valueBtn.value, 10);
     let newValue = currentValue + SCALE_STEP;
-    if (newValue < MIN_SCALE) {
-        newValue = MIN_SCALE;
+    if (newValue > MAX_SCALE) {
+        newValue = MAX_SCALE;
     }
     return scaleImage(newValue);
 };
@@ -26,7 +27,7 @@ function OnBiggerButtonClick() {
 function onSmallerButtonClick() {
     const currentValue = parseInt(valueBtn.value, 10);
     let newValue = currentValue - SCALE_STEP;
-    if (newValue > MIN_SCALE) {
+    if (newValue < MIN_SCALE) {
         newValue = MIN_SCALE;
     }
     return scaleImage(newValue);
