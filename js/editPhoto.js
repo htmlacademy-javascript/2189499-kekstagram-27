1c0465a0a7f8a58f24038ba60ab3f7e19064a8b1
Alex <collagememorycollage@gmail.com>
2022-11-02 23:40:40 +0300

    написали основную логику при нажатии на клавиши для увеличения и уменьшения изображения

--- js/editPhoto.js
+++ js/editPhoto.js
@@ -1,14 +1,35 @@
 const formBtn = document.querySelector('.img-upload__scale');
 const smallerBtn = formBtn.querySelector('.scale__control--smaller');
 const biggerBtn = formBtn.querySelector('.scale__control--bigger');
-const valueBtn = formBtn.querySelector('.scale__control--value');
+let valueBtn = formBtn.querySelector('.scale__control--value').value;
+
 
 
 smallerBtn.addEventListener('click', function() {
-    console.log('- нажат');
+    if (typeof(valueBtn) === 'string') {
+        valueBtn = Number(valueBtn.split('%').join(''));
+    };
+    if (valueBtn > 0) {
+    valueBtn -= 25;
+    };
+    console.log(valueBtn);
+    if (typeof(valueBtn) === 'number') {
+        valueBtn = String(valueBtn + '%');
+        console.log(valueBtn);
+    };
 });
 
 
 biggerBtn.addEventListener('click', function() {
-    console.log('+ нажат');
+    if (typeof(valueBtn) === 'string') {
+        valueBtn = Number(valueBtn.split('%').join(''));
+    };
+    if (valueBtn < 100) {
+    valueBtn += 25;
+    };
+    console.log(valueBtn);
+    if (typeof(valueBtn) === 'number') {
+        valueBtn = String(valueBtn + '%');
+        console.log(valueBtn);
+    };
 });
