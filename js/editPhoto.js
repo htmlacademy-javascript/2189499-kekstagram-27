72307bfc036427b328275e0caf7b6ca1f58330d5
Alex <collagememorycollage@gmail.com>
2022-11-02 21:34:19 +0300

    создали модуль для редоктирования фотографии

---
+++ js/editPhoto.js
@@ -0,0 +1,3 @@
+const formBtn = document.querySelector('.img-upload__scale');
+const smallerBtn = formBtn.querySelector('.scale__control--smaller');
+const biggerBtn = formBtn.querySelector('.scale__control--bigger');
