317c020b0084159066353e0e0c1d26d44c917e62
Alex <collagememorycollage@gmail.com>
2022-11-05 00:34:09 +0300

    исправил ошибки линтера

--- js/editPhoto.js
+++ js/editPhoto.js
@@ -2,7 +2,7 @@ const formBtn = document.querySelector('.img-upload__scale');
 const smallerBtn = formBtn.querySelector('.scale__control--smaller');
 const biggerBtn = formBtn.querySelector('.scale__control--bigger');
 const image = document.querySelector('.img-upload__preview img');
-let valueBtn = formBtn.querySelector('.scale__control--value');
+const valueBtn = formBtn.querySelector('.scale__control--value');
 
 const SCALE_STEP = 25;
 const MIN_SCALE = 25;
@@ -10,36 +10,35 @@ const MAX_SCALE = 100;
 const DEFAULT_SCALE = 100;
 
 function scaleImage (value = DEFAULT_SCALE) {
-    image.style.transform = `scale(${value / 100})`;
-    console.log(image.style.transform);
-    valueBtn.value = `${value}%`;
-};
+  image.style.transform = `scale(${value / 100})`;
+  valueBtn.value = `${value}%`;
+}
 
 function OnBiggerButtonClick() {
-    const currentValue = parseInt(valueBtn.value, 10);
-    let newValue = currentValue + SCALE_STEP;
-    if (newValue > MAX_SCALE) {
-        newValue = MAX_SCALE;
-    }
-    return scaleImage(newValue);
-};
+  const currentValue = parseInt(valueBtn.value, 10);
+  let newValue = currentValue + SCALE_STEP;
+  if (newValue > MAX_SCALE) {
+    newValue = MAX_SCALE;
+  }
+  return scaleImage(newValue);
+}
 
 function onSmallerButtonClick() {
-    const currentValue = parseInt(valueBtn.value, 10);
-    let newValue = currentValue - SCALE_STEP;
-    if (newValue < MIN_SCALE) {
-        newValue = MIN_SCALE;
-    }
-    return scaleImage(newValue);
-};
+  const currentValue = parseInt(valueBtn.value, 10);
+  let newValue = currentValue - SCALE_STEP;
+  if (newValue < MIN_SCALE) {
+    newValue = MIN_SCALE;
+  }
+  return scaleImage(newValue);
+}
 
 function resetScale() {
-    return scaleImage(DEFAULT_SCALE);
-};
+  return scaleImage(DEFAULT_SCALE);
+}
 
 smallerBtn.addEventListener('click', onSmallerButtonClick);
 
 
 biggerBtn.addEventListener('click', OnBiggerButtonClick);
 
-export {resetScale}
+export {resetScale};
