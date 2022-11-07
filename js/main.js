e677d34259575dedcf0f1d2695c3f5953b427842
Alex <collagememorycollage@gmail.com>
2022-11-08 00:03:15 +0300

    получили данные с сервера

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ import './uploadImage.js';
 import './validForm.js';
 import './editPhoto.js';
 import './effect.js';
+
+fetch ('https://27.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
