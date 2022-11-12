264d786810df75ae13636dd0c876a0c046f94b3a
Alex <collagememorycollage@gmail.com>
2022-11-12 20:19:45 +0300

    добавили закрытие при помощи esc

--- js/sendPhoto.js
+++ js/sendPhoto.js
@@ -3,13 +3,20 @@ import { renderSimilarList } from './miniatures.js';
 import { clearHashAndText, setUserFormSubmit } from './validForm.js';
 import { resetModalWindow } from './effect.js';
 import { hideSucsessWindow  } from './validForm.js';
+import { isEscButton } from './utils.js';
 const SIMILAR_WIZARD_COUNT = 10;
 const imgUploadSelectImage = document.getElementById('upload-select-image');
 const idErrorMessage = document.getElementById('error-message');
 
-const removeEventListner = () => {
-  
-}
+
+//ESC
+const onPopupEscKeydown = (evt) => {
+  if (isEscButton(evt)) {
+    evt.preventDefault();
+    hideSucsessWindow();
+  }
+};
+
 
 const showSuccess = () => {
   const sucsessForm = success.content.cloneNode(true);
@@ -22,6 +29,8 @@ const showSuccess = () => {
 
   const section = document.querySelector('.success');
   section.addEventListener('click', hideSucsessWindow);
+
+  window.addEventListener('keydown', onPopupEscKeydown);
 
   resetScale();
   resetModalWindow();
